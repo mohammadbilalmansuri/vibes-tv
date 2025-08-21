586fb01daefec0108b87f552349857b33d4b27ab
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-08-21 16:25:14 +0530

    remove unused icon and image constants

--- constants/images.ts
+++
@@ -1,3 +0,0 @@
-export { default as bg } from "../assets/images/bg.png";
-export { default as highlight } from "../assets/images/highlight.png";
-export { default as rankingGradient } from "../assets/images/rankingGradient.png";
