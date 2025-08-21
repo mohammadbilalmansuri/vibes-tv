5ce25a33eb1a6b7e1cdc1d60320dd9089862eb79
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-08-21 13:29:00 +0530

    add initial project setup with layout, styles, and assets

---
+++ constants/icons.ts
@@ -0,0 +1,8 @@
+export {default as home} from "../assets/icons/home.png";
+export {default as search} from "../assets/icons/search.png";
+export {default as person} from "../assets/icons/person.png";
+export {default as logo} from "../assets/icons/logo.png";
+export {default as save} from "../assets/icons/save.png";
+export {default as star} from "../assets/icons/star.png";
+export {default as play} from "../assets/icons/play.png";
+export {default as arrow} from "../assets/icons/arrow.png";
