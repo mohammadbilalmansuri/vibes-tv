42449d25fec9251cd193f06f4cde6eaf0ff748c1
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-08-21 22:40:44 +0530

    feat: add react-native-svg and transformer, create utility function for class name concatenation

--- metro.config.mjs
+++ metro.config.mjs
@@ -3,4 +3,17 @@ import { withNativeWind } from "nativewind/metro";
 
 const config = getDefaultConfig(__dirname);
 
+const { transformer, resolver } = config;
+
+config.transformer = {
+  ...transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
+};
+
+config.resolver = {
+  ...resolver,
+  assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+  sourceExts: [...resolver.sourceExts, "svg"],
+};
+
 export default withNativeWind(config, { input: "./app/global.css" });
