e1727e69b3190faa0112d8dee103a2c230f48543
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-08-21 23:29:56 +0530

    fix: rename 'active' prop to 'focused' in TabIcon component

--- components/TabIcon.tsx
+++ components/TabIcon.tsx
@@ -2,16 +2,16 @@ import cn from "@/utils/cn";
 import { Image, View } from "react-native";
 
 interface TabIconProps {
-  active: boolean;
+  focused: boolean;
   icon: any;
   title: string;
 }
 
-const TabIcon = ({ active, icon, title }: TabIconProps) => {
+const TabIcon = ({ focused, icon, title }: TabIconProps) => {
   return (
     <View
       className={cn("size-full justify-center items-center mt-4 rounded-full", {
-        "bg-blue-500": active,
+        "bg-blue-500": focused,
       })}
     >
       <Image source={icon} tintColor="#A8B5DB" className="size-5" />
