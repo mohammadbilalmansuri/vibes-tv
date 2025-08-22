303d6eff5491a1fc13538974cd6d709b65194ef7
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-08-22 12:00:58 +0530

    add initial project setup with layout, styles, and assets

---
+++ constants/images.ts
@@ -0,0 +1,3 @@
+export { default as bg } from "../assets/images/bg.png";
+export { default as highlight } from "../assets/images/highlight.png";
+export { default as rankingGradient } from "../assets/images/rankingGradient.png";
