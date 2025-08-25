8143aa4df050e7b4b1807f7823954abc812cf88a
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-08-25 14:05:43 +0530

    fix: remove Appwrite constants and update TMDB API base URL formatting

--- constants.ts
+++ constants.ts
@@ -1,10 +1,3 @@
-export const APPWRITE_ENDPOINT = process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!;
-export const APPWRITE_PROJECT_ID = process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!;
-export const APPWRITE_DATABASE_ID =
-  process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
-export const APPWRITE_COLLECTION_ID =
-  process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
-
 export const TMDB_API_TOKEN = process.env.EXPO_PUBLIC_TMDB_API_TOKEN!;
-export const TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
-export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+export const TMDB_API_BASE_URL = "https://api.themoviedb.org/3/";
+export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/";
