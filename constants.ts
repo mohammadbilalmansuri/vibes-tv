6bbcedc2537c041a1c3671da85ff3967f5ed8539
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-08-27 16:44:44 +0530

    add constants for tab navigation with icons

---
+++ constants.ts
@@ -0,0 +1,24 @@
+import { home, saved, search, user } from "./assets/icons";
+
+export const TABS = [
+  {
+    id: "index",
+    name: "Home",
+    icon: home,
+  },
+  {
+    id: "search",
+    name: "Search",
+    icon: search,
+  },
+  {
+    id: "saved",
+    name: "Saved",
+    icon: saved,
+  },
+  {
+    id: "profile",
+    name: "Profile",
+    icon: user,
+  },
+];
