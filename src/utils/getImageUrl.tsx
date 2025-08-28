1bafcc490bad487079452484deb02ddd24be89d3
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-08-28 17:09:15 +0530

    feat: implement getImageUrl utility function for TMDB image retrieval

---
+++ src/utils/getImageUrl.tsx
@@ -0,0 +1,14 @@
+import { TMDB_IMAGE_BASE_URL } from "@/constants";
+import { ImageSize } from "@/types/tmdb";
+
+/**
+ * Get the full URL for a TMDB image.
+ * @param path - The image path given by tmdb
+ * @param size - The image size key
+ * @returns Fully-qualified image URL
+ */
+export const getImageUrl = (path: string, size: ImageSize) => {
+  if (path.trim() === "") return "";
+  const normalizedPath = path.startsWith("/") ? path.slice(1) : path;
+  return `${TMDB_IMAGE_BASE_URL}/${size}/${normalizedPath}`;
+};
