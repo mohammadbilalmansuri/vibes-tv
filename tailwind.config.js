23c8901a5322efe42c19c21d822c71ebdbbab128
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-09-02 15:55:53 +0530

    refactor: enhance Tailwind CSS configuration with custom colors and animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./src/global.css"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,css}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        default: {
+          50: "#ffffff",
+          100: "#efefef",
+          200: "#dcdcdc",
+          300: "#bdbdbd",
+          400: "#989898",
+          500: "#7c7c7c",
+          600: "#656565",
+          700: "#525252",
+          800: "#464646",
+          900: "#3d3d3d",
+          950: "#292929",
+        },
+      },
+      keyframes: {
+        shimmer: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+      },
+      animation: {
+        shimmer: "shimmer 1s infinite linear",
+      },
+    },
   },
   plugins: [],
 };
