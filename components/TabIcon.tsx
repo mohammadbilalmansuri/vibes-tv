57cd3c09e22985be36001972172842f2162b9874
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-09-05 19:59:05 +0530

    feat: add react-native-svg and transformer, create utility function for class name concatenation

--- components/TabIcon.tsx
+++ components/TabIcon.tsx
@@ -1,22 +1,22 @@
-import { Image, Text, View } from "react-native";
+import cn from "@/utils/cn";
+import { Image, View } from "react-native";
 
-const Tab = ({ focused, icon, title }: any) => {
-  if (focused) {
-    return (
-      <View className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden">
-        <Image source={icon} tintColor="#151312" className="size-5" />
-        <Text className="text-secondary text-base font-semibold ml-2">
-          {title}
-        </Text>
-      </View>
-    );
-  }
+interface TabIconProps {
+  active: boolean;
+  icon: any;
+  title: string;
+}
 
+const TabIcon = ({ active, icon, title }: TabIconProps) => {
   return (
-    <View className="size-full justify-center items-center mt-4 rounded-full">
+    <View
+      className={cn("size-full justify-center items-center mt-4 rounded-full", {
+        "bg-blue-500": active,
+      })}
+    >
       <Image source={icon} tintColor="#A8B5DB" className="size-5" />
     </View>
   );
 };
 
-export default Tab;
+export default TabIcon;
