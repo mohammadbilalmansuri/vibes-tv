4d97b913240221f474063ea740d5ddffb9e90ed7
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-09-08 07:46:31 +0530

    refactor: wrap Tabs component in ScreenView and add Header to screen options

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Tabs } from "expo-router";
-import { TabBar } from "@/components";
+import { Header, ScreenView, TabBar } from "@/components";
 
 export default function TabsLayout() {
   return (
-    <Tabs
-      tabBar={(props) => <TabBar {...props} />}
-      screenOptions={{ headerShown: false, tabBarHideOnKeyboard: true }}
-    >
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="movies" options={{ title: "Movies" }} />
-      <Tabs.Screen name="tv" options={{ title: "TV" }} />
-    </Tabs>
+    <ScreenView>
+      <Tabs
+        tabBar={(props) => <TabBar {...props} />}
+        screenOptions={{
+          header: () => <Header />,
+          tabBarHideOnKeyboard: true,
+        }}
+      >
+        <Tabs.Screen name="index" options={{ title: "Home" }} />
+        <Tabs.Screen name="movies" options={{ title: "Movies" }} />
+        <Tabs.Screen name="tv" options={{ title: "TV" }} />
+      </Tabs>
+    </ScreenView>
   );
 }
