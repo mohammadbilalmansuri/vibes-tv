3a15ececfdd0e11c1358fd3fd4e76e23cc404b5e
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-09-08 09:49:30 +0530

    feat: implement BackHeader and TabHeader components for improved navigation

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-export { default as Header } from "./Header";
 export { default as Logo } from "./Logo";
 export { default as ScreenView } from "./ScreenView";
 export { default as TabBar } from "./TabBar";
+export { default as TabHeader } from "./TabHeader";
+export { default as BackHeader } from "./BackHeader";
