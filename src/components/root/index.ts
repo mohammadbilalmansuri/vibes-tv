8d14b36052eae56107d69a71826b2e55e0c4e393
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-09-09 13:30:30 +0530

    feat: create index file to export root components

---
+++ src/components/root/index.ts
@@ -0,0 +1,5 @@
+export { default as AppProvider } from "./AppProvider";
+export { default as BackHeader } from "./BackHeader";
+export { default as ScreenView } from "./ScreenView";
+export { default as TabBar } from "./TabBar";
+export { default as TabHeader } from "./TabHeader";
