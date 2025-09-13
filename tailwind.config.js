45857e0ab19fae2948c5b81c17ff08ab20a18c01
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-09-14 00:20:29 +0530

    refactor: update Tailwind CSS configuration to use COLORS object for theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+const { COLORS } = require("./theme");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,css}"],
   presets: [require("nativewind/preset")],
-  theme: { extend: {} },
+  theme: { colors: COLORS },
   plugins: [],
 };
