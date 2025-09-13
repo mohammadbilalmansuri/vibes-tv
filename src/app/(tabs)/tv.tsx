28ecdaf4a0c573e70c31787821d64554c93b747d
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-09-14 01:48:54 +0530

    refactor: simplify TV component by removing unused imports and changing layout

--- src/app/(tabs)/tv.tsx
+++ src/app/(tabs)/tv.tsx
@@ -1,27 +1,10 @@
+import { View, Text } from "react-native";
 import React from "react";
-import { Text } from "react-native";
-import { ScreenView } from "@/components";
-import { useGenres, useTvShowCategories, useTvShowsByGenre } from "@/hooks";
 
-export default function Tv() {
-  const {
-    tvGenres,
-    isLoading: genresLoading,
-    errors: genresErrors,
-  } = useGenres();
-  const {
-    airingTodayTvShows,
-    onTheAirTvShows,
-    popularTvShows,
-    topRatedTvShows,
-    isLoading: categoriesLoading,
-    errors: categoriesErrors,
-    refetch: categoriesRefetch,
-  } = useTvShowCategories();
-
+export default function TV() {
   return (
-    <ScreenView inSafeArea={false}>
-      <Text>Tv</Text>
-    </ScreenView>
+    <View>
+      <Text>TV</Text>
+    </View>
   );
 }
