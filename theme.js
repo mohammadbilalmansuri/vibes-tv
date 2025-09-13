2e0fd659a963d4b4f8c4d25b4aef72b7d1cf0751
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-09-14 00:23:00 +0530

    refactor: simplify shark color definitions for consistency and clarity

--- theme.js
+++ theme.js
@@ -3,19 +3,7 @@ const COLORS = {
   yellow: "#eab308",
   green: "#22c55e",
   white: "#ffffff",
-  shark: {
-    50: "#f6f7f9",
-    100: "#eceff2",
-    200: "#d5dbe2",
-    300: "#b0bbc9",
-    400: "#8497ac",
-    500: "#657992",
-    600: "#506179",
-    700: "#424f62",
-    800: "#394453",
-    900: "#333b47",
-    950: "#1c2027",
-  },
+  shark: { primary: "#1c2027", secondary: "#333b47", tertiary: "#424f62" },
 };
 
 module.exports = { COLORS };
