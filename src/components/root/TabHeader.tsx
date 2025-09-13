bed05ac9e99e11d39fbc56496494964c1222c63f
Mohammad Bilal Mansuri <md.bilalmansuri04@gmail.com>
2025-09-14 01:30:18 +0530

    refactor: update BackHeader and TabHeader to use consistent padding and icon sizes

--- src/components/root/TabHeader.tsx
+++ src/components/root/TabHeader.tsx
@@ -3,22 +3,21 @@ import { View, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
 import { Search } from "lucide-react-native";
 import { COLORS } from "@/constants";
-import Logo from "../ui/Logo";
+import { Logo } from "../ui";
 
 const TabHeader = () => {
   const router = useRouter();
-
   const handleSearchPress = () => router.push("/search");
 
   return (
-    <View className="px-5 py-2 flex-row justify-between items-center bg-shark-primary">
+    <View className="px-5 py-2.5 flex-row justify-between items-center bg-shark-primary">
       <Logo />
       <TouchableOpacity
         onPress={handleSearchPress}
         className="p-2 justify-center items-center bg-shark-tertiary rounded-full"
         activeOpacity={0.6}
       >
-        <Search size={16} color={COLORS.white} />
+        <Search size={18} color={COLORS.white} />
       </TouchableOpacity>
     </View>
   );
