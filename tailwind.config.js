9978dfefbf8252f0b8b7432e7d60fdeb7bd2d52c
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-10-02 12:05:48 +0530

    fix: remove unused color extension from Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,7 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-      colors: {
-        base: {
-          100: "#A8B5DB",
-          200: "#D6C7FF",
-          300: "#AB8BFF",
-          400: "#221F3D",
-          500: "#0F0D23",
-          600: "#030014",
-          text: "#9CA4AB",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
