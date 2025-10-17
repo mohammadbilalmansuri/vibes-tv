1d493fa763f01639001ddf2e7fe5cb7cdea4738a
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-10-17 20:09:44 +0530

    fix: update TMDB API base URLs to remove trailing slashes

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const TMDB_API_TOKEN = process.env.EXPO_PUBLIC_TMDB_API_TOKEN!;
-export const TMDB_API_BASE_URL = "https://api.themoviedb.org/3/";
-export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/";
+export const TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
+export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
 
 export { default as LOGO } from "../assets/logo.png";
