4003afa56afd544fdf7d4449df00e1a0ed454612
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-10-29 16:19:31 +0530

    refactor: organize types

---
+++ src/types/tmdb/index.ts
@@ -0,0 +1,4 @@
+export * from "./common";
+export * from "./movies";
+export * from "./search";
+export * from "./tv";
---
+++ src/types/tmdb/tv.ts
@@ -0,0 +1,162 @@
+import {
+  Genre,
+  Network,
+  ProductionCompany,
+  ProductionCountry,
+  SpokenLanguage,
+  TMDBResponse,
+} from "./common";
+
+export interface BaseTVShow {
+  backdrop_path?: string | null;
+  first_air_date: string;
+  genre_ids: number[];
+  id: number;
+  name: string;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path?: string | null;
+  vote_average: number;
+  vote_count: number;
+}
+
+export type TrendingTVResponse = TMDBResponse<
+  BaseTVShow & { adult: boolean; media_type: string }
+>;
+export type TVShowsAiringTodayResponse = TMDBResponse<BaseTVShow>;
+export type TVShowsOnTheAirResponse = TMDBResponse<BaseTVShow>;
+export type PopularTVShowsResponse = TMDBResponse<BaseTVShow>;
+export type TopRatedTVShowsResponse = TMDBResponse<BaseTVShow>;
+export type SearchTVShowsResponse = TMDBResponse<
+  BaseTVShow & { adult: boolean }
+>;
+export type DiscoverTVShowsResponse = TMDBResponse<BaseTVShow>;
+
+export interface Creator {
+  id: number;
+  credit_id: string;
+  name: string;
+  gender: number;
+  profile_path?: string | null;
+}
+
+export interface CrewMember {
+  department: string;
+  job: string;
+  credit_id: string;
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path?: string | null;
+}
+
+export interface GuestStar {
+  character: string;
+  credit_id: string;
+  order: number;
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path?: string | null;
+}
+
+export interface LastEpisodeToAir {
+  id: number;
+  name: string;
+  overview: string;
+  vote_average: number;
+  vote_count: number;
+  air_date: string;
+  episode_number: number;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  show_id: number;
+  still_path?: string | null;
+}
+
+export interface Season {
+  air_date: string;
+  episode_count: number;
+  id: number;
+  name: string;
+  overview: string;
+  poster_path?: string | null;
+  season_number: number;
+  vote_average: number;
+}
+
+export interface TVShowDetailResponse {
+  adult: boolean;
+  backdrop_path?: string | null;
+  created_by: Creator[];
+  episode_run_time: number[];
+  first_air_date: string;
+  genres: Genre[];
+  homepage?: string | null;
+  id: number;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: LastEpisodeToAir;
+  name: string;
+  next_episode_to_air?: string | null;
+  networks: Network[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  origin_country: string[];
+  original_language: string;
+  original_name: string;
+  overview: string;
+  popularity: number;
+  poster_path?: string | null;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  seasons: Season[];
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline?: string | null;
+  type: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+export interface Episode {
+  air_date: string;
+  episode_number: number;
+  id: number;
+  name: string;
+  overview: string;
+  production_code: string;
+  runtime: number;
+  season_number: number;
+  show_id: number;
+  still_path?: string | null;
+  vote_average: number;
+  vote_count: number;
+  crew: CrewMember[];
+  guest_stars: GuestStar[];
+}
+
+export interface TVSeasonDetailResponse {
+  _id: string;
+  air_date: string;
+  episodes: Episode[];
+  name: string;
+  overview: string;
+  id: number;
+  poster_path?: string | null;
+  season_number: number;
+  vote_average: number;
+}
