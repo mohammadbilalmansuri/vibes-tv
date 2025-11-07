164f0501467f5e43920823011cb72c81bf11a710
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-11-07 19:49:01 +0530

    feat: create index file for hooks to streamline exports

---
+++ src/hooks/index.ts
@@ -0,0 +1,12 @@
+export { default as useDebounce } from "./useDebounce";
+
+export { default as useGenres } from "./useGenres";
+export { default as useMovieDetails } from "./useMovieDetails";
+export { default as useMovies } from "./useMovies";
+export { default as useMoviesByGenre } from "./useMoviesByGenre";
+export { default as useSearch } from "./useSearch";
+export { default as useTrending } from "./useTrending";
+export { default as useTv } from "./useTv";
+export { default as useTvByGenre } from "./useTvByGenre";
+export { default as useTVDetails } from "./useTVDetails";
+export { default as useTVSeasonDetails } from "./useTVSeasonDetails";
