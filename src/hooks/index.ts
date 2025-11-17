8a82be9d7c23ad356bbf954e993348f0eb047712
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-11-17 11:46:36 +0530

    refactor: update index.ts to include missing exports for movie categories and TV show genres

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,12 +1,11 @@
 export { default as useDebounce } from "./useDebounce";
-
 export { default as useGenres } from "./useGenres";
+export { default as useMovieCategories } from "./useMovieCategories";
 export { default as useMovieDetails } from "./useMovieDetails";
-export { default as useMovies } from "./useMovies";
 export { default as useMoviesByGenre } from "./useMoviesByGenre";
 export { default as useSearch } from "./useSearch";
 export { default as useTrending } from "./useTrending";
-export { default as useTv } from "./useTv";
-export { default as useTvByGenre } from "./useTvByGenre";
 export { default as useTVDetails } from "./useTVDetails";
 export { default as useTVSeasonDetails } from "./useTVSeasonDetails";
+export { default as useTvShowCategories } from "./useTvShowCategories";
+export { default as useTvShowsByGenre } from "./useTvShowsByGenre";
