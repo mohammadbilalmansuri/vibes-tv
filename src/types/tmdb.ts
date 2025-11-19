49cd8f153a190eba6e9188a293877dded07c4df7
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-11-19 18:02:03 +0530

    refactor: rename MultiSearchResponse to SearchResponse and update type definitions for consistency

--- src/types/tmdb.ts
+++ src/types/tmdb.ts
@@ -288,29 +288,24 @@ export interface TVSeasonDetailResponse {
 
 export type SearchMode = "multi" | "movie" | "tv";
 
-export type MultiSearchResponse = TMDBResponse<{
+export type SearchResponse = TMDBResponse<{
   adult: boolean;
-  backdrop_path?: string | null;
-  id: number;
-  title?: string;
-  name?: string;
-  original_language: string;
-  original_title?: string;
-  original_name?: string;
-  overview: string;
-  poster_path?: string | null;
-  media_type: string;
-  genre_ids: number[];
-  popularity: number;
-  release_date?: string;
+  backdrop_path: string;
   first_air_date?: string;
+  genre_ids: number[];
+  id: number;
+  media_type?: string;
+  name?: string;
+  origin_country?: string[];
+  original_language: string;
+  original_name?: string;
+  original_title?: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date?: string;
+  title?: string;
   video?: boolean;
   vote_average: number;
   vote_count: number;
 }>;
-
-export type SearchMoviesResponse = TMDBResponse<BaseMovie>;
-
-export type SearchTVShowsResponse = TMDBResponse<
-  BaseTVShow & { adult: boolean }
->;
