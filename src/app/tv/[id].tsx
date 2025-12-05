59177675011599bdbb2bac448ec7bc87f34a2c56
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-12-05 14:26:52 +0530

    feat: add Movie and TvShow components for displaying content

---
+++ src/app/tv/[id].tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function TvShow() {
+  return (
+    <View>
+      <Text>TvShow</Text>
+    </View>
+  );
+}
