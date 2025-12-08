dd50ff1875dfe7b3aca24c5f51c7400ece659398
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-12-08 19:00:47 +0530

    refactor: replace View with ScreenView for consistent layout across components

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,10 +1,11 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { Text } from "react-native";
+import { ScreenView } from "@/components";
 
 export default function Search() {
   return (
-    <View>
+    <ScreenView>
       <Text>Search</Text>
-    </View>
+    </ScreenView>
   );
 }
