34933974127bbafd90f53c44af9d68fc3a600779
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-12-20 18:49:41 +0530

    feat: implement BackHeader and TabHeader components for improved navigation

---
+++ src/components/BackHeader.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+import { ArrowLeft } from "lucide-react-native";
+import Logo from "./Logo";
+import { DEFAULT_COLORS } from "@/constants";
+
+const BackHeader = () => {
+  const router = useRouter();
+  const handleBackPress = () => router.back();
+
+  return (
+    <View className="px-5 py-2 flex-row justify-between items-center bg-default-950">
+      <TouchableOpacity
+        onPress={handleBackPress}
+        className="p-2 justify-center items-center bg-default-800 rounded-full"
+        activeOpacity={0.5}
+      >
+        <ArrowLeft size={16} color={DEFAULT_COLORS[300]} />
+      </TouchableOpacity>
+      <Logo />
+      <View className="w-10" />
+    </View>
+  );
+};
+
+export default BackHeader;
---
+++ src/components/TabHeader.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, TouchableOpacity } from "react-native";
+import { useRouter } from "expo-router";
+import { Search } from "lucide-react-native";
+import { DEFAULT_COLORS } from "@/constants";
+import Logo from "./Logo";
+
+const TabHeader = () => {
+  const router = useRouter();
+
+  const handleSearchPress = () => router.push("/search");
+
+  return (
+    <View className="px-5 py-2 flex-row justify-between items-center bg-default-950">
+      <Logo />
+      <TouchableOpacity
+        onPress={handleSearchPress}
+        className="p-2 justify-center items-center bg-default-800 rounded-full"
+        activeOpacity={0.5}
+      >
+        <Search size={16} color={DEFAULT_COLORS[300]} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default TabHeader;
