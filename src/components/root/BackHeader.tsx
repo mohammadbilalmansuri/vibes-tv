f3cdfcdefcbb5997a2d9f3276921e94da971353c
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2025-12-24 21:33:08 +0530

    move header and tabbar component to root dir

--- src/components/root/BackHeader.tsx
+++ src/components/root/BackHeader.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { View, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
-import { Search } from "lucide-react-native";
+import { ArrowLeft } from "lucide-react-native";
+import Logo from "../Logo";
 import { DEFAULT_COLORS } from "@/constants";
-import Logo from "./Logo";
 
-const TabHeader = () => {
+const BackHeader = () => {
   const router = useRouter();
-
-  const handleSearchPress = () => router.push("/search");
+  const handleBackPress = () => router.back();
 
   return (
     <View className="px-5 py-2 flex-row justify-between items-center bg-default-950">
-      <Logo />
       <TouchableOpacity
-        onPress={handleSearchPress}
+        onPress={handleBackPress}
         className="p-2 justify-center items-center bg-default-800 rounded-full"
         activeOpacity={0.5}
       >
-        <Search size={16} color={DEFAULT_COLORS[300]} />
+        <ArrowLeft size={16} color={DEFAULT_COLORS[300]} />
       </TouchableOpacity>
+      <Logo />
+      <View className="w-10" />
     </View>
   );
 };
 
-export default TabHeader;
+export default BackHeader;
--- src/components/root/TabHeader.tsx
+++ src/components/root/TabHeader.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 import { View, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
-import { ArrowLeft } from "lucide-react-native";
-import Logo from "./Logo";
+import { Search } from "lucide-react-native";
 import { DEFAULT_COLORS } from "@/constants";
+import Logo from "../Logo";
 
-const BackHeader = () => {
+const TabHeader = () => {
   const router = useRouter();
-  const handleBackPress = () => router.back();
+
+  const handleSearchPress = () => router.push("/search");
 
   return (
     <View className="px-5 py-2 flex-row justify-between items-center bg-default-950">
+      <Logo />
       <TouchableOpacity
-        onPress={handleBackPress}
+        onPress={handleSearchPress}
         className="p-2 justify-center items-center bg-default-800 rounded-full"
         activeOpacity={0.5}
       >
-        <ArrowLeft size={16} color={DEFAULT_COLORS[300]} />
+        <Search size={16} color={DEFAULT_COLORS[300]} />
       </TouchableOpacity>
-      <Logo />
-      <View className="w-10" />
     </View>
   );
 };
 
-export default BackHeader;
+export default TabHeader;
