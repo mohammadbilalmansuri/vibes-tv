edebc7e2841562388b5be6ed74c054699dcce625
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-01-09 09:22:15 +0530

    fix: enhance layout and styling of OfflineScreen component for better user experience

--- src/components/root/OfflineScreen.tsx
+++ src/components/root/OfflineScreen.tsx
@@ -5,12 +5,12 @@ import { DEFAULT_COLORS } from "@/constants";
 
 const OfflineScreen = () => {
   return (
-    <View className="flex-1 items-center justify-center gap-4 p-4">
-      <WifiOff size={48} color={DEFAULT_COLORS.accent} />
-      <Text className="text-xl font-semibold text-default-50">
+    <View className="flex-1 items-center justify-center gap-6 p-4">
+      <WifiOff size={48} color={DEFAULT_COLORS.accent} strokeWidth={1.5} />
+      <Text className="text-xl font-semibold text-default-50 text-center">
         You’re Offline
       </Text>
-      <Text className="text-default-200 text-center">
+      <Text className="text-default-500 text-center -mt-3">
         Please check your internet connection.
       </Text>
     </View>
