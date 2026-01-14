6c4e9e21711eb9a32b17d51e571d04b575730125
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-01-14 17:55:50 +0530

    refactor: remove default color definitions from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx,css}"],
   presets: [require("nativewind/preset")],
-  theme: {
-    extend: {
-      colors: {
-        default: {
-          50: "#fafafa",
-          100: "#f4f4f5",
-          200: "#e8e8ea",
-          300: "#dcdce0",
-          400: "#d4d4d8",
-          500: "#a1a1aa",
-          600: "#46484e",
-          700: "#383b43",
-          800: "#2d3138",
-          900: "#23262d",
-          950: "#1c2027",
-          accent: "#f3506c",
-        },
-      },
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 };
