ebe4b314a64410d96c0d2b64a59b0ca98dc4d3fe
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-01-14 15:40:37 +0530

    refactor: remove unused logo import and default colors from constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,3 @@
-export { default as LOGO } from "../assets/logo.png";
-
 export const TMDB_API_TOKEN = process.env.EXPO_PUBLIC_TMDB_API_TOKEN!;
 export const TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
 export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
@@ -41,19 +39,6 @@ export const GENRE_DISCOVER_CACHE_CONFIG = {
   gcTime: 24 * 60 * 60 * 1000,
 };
 
-// Default Colors
+// Theme Colors
 
-export const DEFAULT_COLORS = {
-  50: "#fafafa",
-  100: "#f4f4f5",
-  200: "#e8e8ea",
-  300: "#dcdce0",
-  400: "#d4d4d8",
-  500: "#a1a1aa",
-  600: "#46484e",
-  700: "#383b43",
-  800: "#2d3138",
-  900: "#23262d",
-  950: "#1c2027",
-  accent: "#f3506c",
-};
+export { rose as COLOR_ROSE, gray as COLOR_GRAY } from "tailwindcss/colors";
