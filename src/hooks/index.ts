aaa1ea0415d066b0d878e4afe768789aebf4a6e5
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-01-25 22:07:30 +0530

    refactor: reorganize hook exports for improved structure and clarity

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,6 @@
 export { default as useDebounce } from "./useDebounce";
 export { default as useGenres } from "./useGenres";
-export { default as useMovieCategories } from "./useMovieCategories";
-export { default as useMovieDetails } from "./useMovieDetails";
-export { default as useMoviesByGenre } from "./useMoviesByGenre";
 export { default as useSearch } from "./useSearch";
-export { default as useTrending } from "./useTrending";
-export { default as useTvSeasonDetails } from "./useTvSeasonDetails";
-export { default as useTvShowCategories } from "./useTvShowCategories";
-export { default as useTvShowDetails } from "./useTvShowDetails";
-export { default as useTvShowsByGenre } from "./useTvShowsByGenre";
+export { default as useHomeQueries } from "./useHomeQueries";
+export * from "./useMovieQueries";
+export * from "./useTvQueries";
