ab9859297a780dcd784521be0e64482bb3a5edc6
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-02-01 20:18:05 +0530

    refactor: update TabHeader to use COLORS for styling and improve background consistency

--- src/components/root/TabHeader.tsx
+++ src/components/root/TabHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { View, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
 import { Search } from "lucide-react-native";
-import { DEFAULT_COLORS } from "@/constants";
+import { COLORS } from "@/constants";
 import Logo from "../ui/Logo";
 
 const TabHeader = () => {
@@ -11,14 +11,14 @@ const TabHeader = () => {
   const handleSearchPress = () => router.push("/search");
 
   return (
-    <View className="px-5 py-2 flex-row justify-between items-center bg-default-950">
+    <View className="px-5 py-2 flex-row justify-between items-center bg-shark-primary">
       <Logo />
       <TouchableOpacity
         onPress={handleSearchPress}
-        className="p-2 justify-center items-center bg-default-800 rounded-full"
-        activeOpacity={0.5}
+        className="p-2 justify-center items-center bg-shark-tertiary rounded-full"
+        activeOpacity={0.6}
       >
-        <Search size={16} color={DEFAULT_COLORS[300]} />
+        <Search size={16} color={COLORS.white} />
       </TouchableOpacity>
     </View>
   );
