4fb5c358ef5f94474e34aa4d81f003a038a01ee8
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-02-03 21:03:46 +0530

    refactor: update BackHeader and TabHeader to use consistent padding and icon sizes

--- src/components/root/BackHeader.tsx
+++ src/components/root/BackHeader.tsx
@@ -3,21 +3,20 @@ import { View, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
 import { ArrowLeft } from "lucide-react-native";
 import { COLORS } from "@/constants";
-import Logo from "../ui/Logo";
+import { Logo } from "../ui";
 
 const BackHeader = () => {
   const router = useRouter();
-
   const handleBackPress = () => router.back();
 
   return (
-    <View className="px-5 py-2 flex-row justify-between items-center bg-shark-primary">
+    <View className="px-5 py-2.5 flex-row justify-between items-center bg-shark-primary">
       <TouchableOpacity
         onPress={handleBackPress}
         className="p-2 justify-center items-center bg-shark-tertiary rounded-full"
         activeOpacity={0.6}
       >
-        <ArrowLeft size={16} color={COLORS.white} />
+        <ArrowLeft size={18} color={COLORS.white} />
       </TouchableOpacity>
       <Logo />
       <View className="w-10" />
