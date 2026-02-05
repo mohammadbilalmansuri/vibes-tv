de5bda776581e3a9f156376969b893c761639ccf
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-02-05 17:48:59 +0530

    refactor: update OfflineScreen to use ScreenView for improved layout consistency

--- src/components/root/OfflineScreen.tsx
+++ src/components/root/OfflineScreen.tsx
@@ -2,20 +2,21 @@ import React from "react";
 import { View, Text } from "react-native";
 import { WifiOff } from "lucide-react-native";
 import { COLORS } from "@/constants";
+import ScreenView from "./ScreenView";
 
 const OfflineScreen = () => {
   return (
-    <View className="flex-1 items-center justify-center gap-5 p-5">
+    <ScreenView className="items-center justify-center gap-5 p-5">
       <View className="bg-shark-secondary p-4 rounded-full">
-        <WifiOff size={28} color={COLORS.yellow} strokeWidth={2} />
+        <WifiOff size={28} color={COLORS.yellow} />
       </View>
       <Text className="text-xl font-semibold text-white text-center">
         You’re Offline
       </Text>
-      <Text className="text-white/60 text-center -mt-3">
+      <Text className="text-white/60 text-center -mt-2">
         Please check your internet connection.
       </Text>
-    </View>
+    </ScreenView>
   );
 };
 
