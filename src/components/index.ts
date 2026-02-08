a800169d25dea469a3907a4fd2fb4ef2c6615e75
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-02-08 13:53:31 +0530

    refactor: remove unused exports from components index file

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Logo } from "./Logo";
-export { default as ScreenView } from "./ScreenView";
-export { default as TabBar } from "./TabBar";
-export { default as TabHeader } from "./TabHeader";
-export { default as BackHeader } from "./BackHeader";
-export { default as Skeleton } from "./Skeleton";
