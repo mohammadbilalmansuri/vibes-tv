862df1ba446a76b6baad21d3e15e1186f4c2ce03
Mohammad Bilal Mansuri <mbilalmansuri10@gmail.com>
2026-02-10 17:49:03 +0530

    refactor: simplify Movies component by removing unused hooks and changing layout

--- src/app/(tabs)/movies.tsx
+++ src/app/(tabs)/movies.tsx
@@ -1,26 +1,10 @@
 import React from "react";
-import { Text } from "react-native";
-import { ScreenView } from "@/components";
-import { useGenres, useMovieCategories, useMoviesByGenre } from "@/hooks";
+import { View, Text } from "react-native";
 
 export default function Movies() {
-  const {
-    movieGenres,
-    isLoading: genresLoading,
-    errors: genresErrors,
-  } = useGenres();
-  const {
-    popularMovies,
-    topRatedMovies,
-    upcomingMovies,
-    nowPlayingMovies,
-    isLoading: categoriesLoading,
-    errors: categoriesErrors,
-  } = useMovieCategories();
-
   return (
-    <ScreenView inSafeArea={false}>
+    <View>
       <Text>Movies</Text>
-    </ScreenView>
+    </View>
   );
 }
